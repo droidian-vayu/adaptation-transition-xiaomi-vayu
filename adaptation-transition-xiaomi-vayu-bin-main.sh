0cf8ba4f6a8ad43dc170b322a8b63ebfc528a0df
berbascum <berbascum@ticv.cat>
2024-06-19 01:05:24 +0200

    Update: install scripts to new tests

--- adaptation-transition-xiaomi-vayu-bin-main.sh
+++ adaptation-transition-xiaomi-vayu-bin-main.sh
@@ -9,19 +9,14 @@
 # All rights reserved.
 #
 # BSD 3-Clause License
+
+
 #[HEADER_SECTION]
 #[HEADER_END]
 
 set -e
 
 [ -z $(dpkg -l | grep "adaptation-droidian-vayu") ]  && echo "adaptation-droidian-vayu is not installed!" && exit 0
-
-## Patch the vayu.list to the new repo
-old_vayu_list="/usr/lib/adaptation-droidian-vayu/sources.list.d/vayu.list"
-[ ! -f "${old_vayu_list}" ] && echo "Old vayu.list file not found!" && exit 1
-cat /usr/share/keyrings/vayu-new.gpg > /usr/share/keyrings/vayu.gpg 
-sed -i '/vacuumbeef/droidian-vayu/g' "${old_vayu_list}"
-sed -i '/vayu-repo/apt-xiaomi-vayu/g' "${old_vayu_list}"
 
 ## Update repos
 apt-get update
@@ -33,6 +28,9 @@ apt-get --purge remove -y adaptation-droidian-vayu adaptation-vayu-configs
 
 # Install the new adaptation packages
 apt-get install -y adaptation-xiaomi-vayu adaptation-xiaomi-vayu-configs
+
+## Update repos
+apt-get update
 
 # Remove the transition package 
 apt-get --purge remove -y adaptation-transition-xiaomi-vayu
