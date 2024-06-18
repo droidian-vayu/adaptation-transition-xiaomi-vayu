9bb7ed357cd39ea55fa7f5db2a152c3bb6ee8147
berbascum <berbascum@ticv.cat>
2024-06-18 22:57:51 +0200

    Update: install scripts to get debug information

--- adaptation-transition-xiaomi-vayu-bin-main.sh
+++ adaptation-transition-xiaomi-vayu-bin-main.sh
@@ -5,6 +5,8 @@
 
 set -e
 
+[ -z $(dpkg -l | grep "adaptation-droidian-vayu") ]  && echo "adaptation-droidian-vayu is not installed!" && exit 0
+
 # Upgrade old adaptation-droidian-vayu and adaptation-vayu-configs packages to a non dependent desinstalable version
 apt-get update && apt-get install -y adaptation-droidian-vayu adaptation-vayu-configs
 
@@ -17,4 +19,3 @@ apt-get install -y adaptation-xiaomi-vayu adaptation-xiaomi-vayu-configs
 # Remove the transition package 
 apt-get --purge remove -y adaptation-transition-xiaomi-vayu
 
-exit 0
