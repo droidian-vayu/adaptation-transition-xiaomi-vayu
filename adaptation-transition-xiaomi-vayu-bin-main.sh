e0dd2d0386d29fba486e7e438fa4884e40c6ec46
berbascum <berbascum@ticv.cat>
2024-06-18 22:10:38 +0200

    Fix: the install scripts are broken, testing a new method

--- adaptation-transition-xiaomi-vayu-bin-main.sh
+++ adaptation-transition-xiaomi-vayu-bin-main.sh
@@ -3,6 +3,8 @@
 #[HEADER_SECTION]
 #[HEADER_END]
 
+set -e
+
 # Upgrade old adaptation-droidian-vayu and adaptation-vayu-configs packages to a non dependent desinstalable version
 apt-get update && apt-get install -y adaptation-droidian-vayu adaptation-vayu-configs
 
@@ -12,5 +14,7 @@ apt-get --purge remove -y adaptation-droidian-vayu adaptation-vayu-configs
 # Install the new adaptation packages
 apt-get install -y adaptation-xiaomi-vayu adaptation-xiaomi-vayu-configs
 
+# Remove the transition package 
+apt-get --purge remove -y adaptation-transition-xiaomi-vayu
 
 exit 0
